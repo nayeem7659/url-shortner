47be89cf54720830a3963ca1e4eadb81c8b4f8d0
Naim AK <naimak659@gmail.com>
2024-06-16 15:33:53 +0600

    still trying

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
+
 import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
 
-// start
-
 dotenv.config();
-
-app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.CORS_ORIGIN}` || "*",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 // app.use(express.urlencoded())
